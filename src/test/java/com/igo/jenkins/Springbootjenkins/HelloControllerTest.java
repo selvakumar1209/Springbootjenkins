859c65485adb5379ca5c3249089ed1bd93d2f6bb
selvakumar1209 <159786158+selvakumar1209@users.noreply.github.com>
2024-12-02 12:23:51 +0530

    First commit

---
+++ src/test/java/com/igo/jenkins/Springbootjenkins/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.igo.jenkins.Springbootjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testSayHello() throws Exception {
+    	
+    	System.out.println("The helloController test was excecuting");
+        mockMvc.perform(get("/api/hello"))
+               .andExpect(status().isOk())  
+               .andExpect(content().string("Hello jenkins")); 
+    }
+}
