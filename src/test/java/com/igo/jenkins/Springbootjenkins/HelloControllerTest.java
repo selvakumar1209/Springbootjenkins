87bb7e75f45df844b2b799169805961dc23b3c2c
selvakumar1209 <159786158+selvakumar1209@users.noreply.github.com>
2024-12-02 17:07:51 +0530

    Pipline failed

--- src/test/java/com/igo/jenkins/Springbootjenkins/HelloControllerTest.java
+++ src/test/java/com/igo/jenkins/Springbootjenkins/HelloControllerTest.java
@@ -18,7 +18,7 @@ class HelloControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    void testSayHello() throws Exception {
+    testSayHello() throws Exception {
     	
     	System.out.println("The helloController test was Dev");
     	System.out.println("The helloController test was Dev");
